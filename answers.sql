bc0f6a618cc0af0a065c245cf173563fe9ae3f88
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-10-20 09:33:27 -0600

    Finished problems 4-6

--- answers.sql
+++ answers.sql
@@ -23,6 +23,25 @@ WHERE processed = 'f';
 -- Problem 4
 SELECT name, SUM(num_cupcakes) as sum
 FROM cupcakes c
-    JOIN orders o ON o.cupcake_id = c.id
+    LEFT JOIN orders o ON o.cupcake_id = c.id
 GROUP BY name
 ORDER BY name ASC;
+
+
+-- Problem 5
+SELECT email, sum(num_cupcakes) as total
+FROM customers c
+    JOIN orders o ON c.id = o.customer_id
+GROUP BY email
+ORDER BY total DESC;
+
+
+-- Problem 6
+SELECT DISTINCT fname, lname, email
+FROM customers c
+    JOIN orders o ON c.id = o.customer_id
+    JOIN cupcakes ck ON o.cupcake_id = ck.id
+WHERE 
+    processed = 't' AND
+    name = 'funfetti'
+;
