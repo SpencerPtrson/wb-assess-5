8de1f00a5d9ca14febe7ec1b07b780c4af3ead06
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-10-20 14:21:22 -0600

    Added awaits to the query problems for the sake of it

--- src/queries.js
+++ src/queries.js
@@ -3,40 +3,40 @@ import { Animal, Human } from './model.js';
 // const { Animal, Human } = await import('./src/model.js'); // Used for node testing
 
 // Get the human with the primary key 2
-export const query1 = Human.findByPk(2);
+export const query1 = await Human.findByPk(2);
 
 // Get the first animal whose species is "fish"
-export const query2 = Animal.findOne({
+export const query2 = await Animal.findOne({
     where: { species: 'fish' }
 });
 
 // Get all animals belonging to the human with primary key 5
-export const query3 = Animal.findAll({
+export const query3 = await Animal.findAll({
     where: { humanId: 5 }
 });
 
 // Get all animals born in a year greater than (but not equal to) 2015.
-export const query4 = Animal.findAll({
+export const query4 = await Animal.findAll({
     where: { birthYear: { [Op.gt]: 2015 } }
 });
 
 // Get all the humans with first names that start with "J"
-export const query5 = Human.findAll({
+export const query5 = await Human.findAll({
     where: { fname: { [Op.startsWith]: 'J' } }
 });
 
 // Get all the animals who don't have a birth year
-export const query6 = Animal.findAll({
+export const query6 = await Animal.findAll({
     where: { birthYear: { [Op.is]: null } }
 });
 
 // Get all the animals with species "fish" OR "rabbit"
-export const query7 = Animal.findAll({
+export const query7 = await Animal.findAll({
     where: { species: { [Op.or]: ['fish', 'rabbit'] } }
 });
 
 // Get all the humans who DON'T have an email address that contains "gmail"
-export const query8 = Human.findAll({
+export const query8 = await Human.findAll({
     where: { email: { [Op.notLike]: '%gmail%' } }
 });
 
